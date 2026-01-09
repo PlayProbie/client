f160950367150129a28bdec1a1fe909a19784cde
Robin <54692541+developerQuo@users.noreply.github.com>
2026-01-09 10:07:51 +0900

    Hotfix v4 (#54)
    * fix: 게임 목록 화면 이동 불가 이슈
    
    * fix: 게임 목록 이동

--- src/components/layout/status-widget/UploadStatusWidget.tsx
+++ src/components/layout/status-widget/UploadStatusWidget.tsx
@@ -94,7 +94,7 @@ export function UploadStatusWidget() {
   };
 
   return (
-    <StatusWidgetContainer bottomPosition="bottom-4">
+    <StatusWidgetContainer bottomPosition="bottom-8">
       <StatusWidgetHeader
         icon={<Upload className="size-4" />}
         title="업로드"
